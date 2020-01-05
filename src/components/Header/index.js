455738a5a564274131c52ec5ed19d8c88efbda10
ugesh <ugesh.roopchand@hotmail.com>
2020-01-05 11:55:46 +0400

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from'react';
+
+const Header = props => {
+    return (
+        <header className="header">
+         <nav className="HeaderMenu">
+             <a href="#">Home</a>
+             <a href="#">About us</a>
+             <a href="#">Contact us</a>
+         </nav>
+         <div>
+             social Media Icons
+         </div>
+        </header>
+    );
+}
+
+export default Header;
+
